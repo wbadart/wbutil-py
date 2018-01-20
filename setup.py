2f12b0a6c9f398621b93d89ca77b9e4c4cafe9b8
Will Badart <wbadart@live.com>
2018-01-19 23:58:36 -0500

    Tweaked version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='wbutil',
-      version='0.0.2a0',
+      version='0.0.1a1',
       packages=find_packages(),
       url='https://github.com/wbadart/wbutil-py',
 
