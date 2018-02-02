2d04f432a2ea321f8d3d36d0eb1c5c6dc531df20
Will Badart <wbadart@live.com>
2018-02-02 13:46:56 -0500

    Version bump for timeout

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='wbutil',
-      version='0.0.1a6',
+      version='0.0.1a7',
       packages=find_packages(),
       url='https://github.com/wbadart/wbutil-py',
 
